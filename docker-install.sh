449cec4859180708c3238aa101e0eecd747b30c3
nicolas701 <96244211+nicolas701@users.noreply.github.com>
2023-01-02 19:19:26 -0300

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
+## Install Docker Engine on Ubuntu/Debian 
 # https://docs.docker.com/engine/install/ubuntu/
 
-## 1. Update the apt package index and install packages to allow apt to use a repository over HTTPS:
+## 1. Update the apt package index and install packages to allow apt to use a repository over HTTPS
+echo "[INFO] Update the apt package index and install packages to allow apt to use a repository over HTTPS"
 sudo apt update
 sudo apt install ca-certificates curl gnupg lsb-release
 
-## 2. Add Docker’s official GPG key:
+## 2. Add Docker’s official GPG key
+echo "[INFO] Add Docker’s official GPG key"
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-## 3. Use the following command to set up the repository:
+## 3. Use the following command to set up the repository
+echo "[INFO] Set up the repository"
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-## 4. Update the apt package index:
+## 4. Update the apt package index
+echo "[INFO] Update the apt package index"
 sudo update
 
-## 5. Install Docker Engine, containerd, and Docker Compose.
+## 5. Install Docker Engine, containerd, and Docker Compose
+echo "[INFO] Install Docker Engine, containerd, and Docker Compose"
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose
 
-## 6.Add your user to the docker group.
+## 6. Add your user to the docker group
+echo "[INFO] Add your user to the docker group"
 sudo usermod -aG docker $USER

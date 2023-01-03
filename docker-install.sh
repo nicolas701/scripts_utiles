6f721ec6ccecdcc68dbc85a2428fe9b821a912ba
nicolas701 <96244211+nicolas701@users.noreply.github.com>
2023-01-03 09:10:36 -0300

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -3,6 +3,14 @@
 ## Install Docker Engine on Ubuntu/Debian 
 # https://docs.docker.com/engine/install/ubuntu/
 
+## OS requirements
+# To install Docker Engine, you need the 64-bit version of one of these Ubuntu versions:
+#   Ubuntu Kinetic 22.10
+#   Ubuntu Jammy 22.04 (LTS)
+#   Ubuntu Focal 20.04 (LTS)
+#   Ubuntu Bionic 18.04 (LTS)
+# Docker Engine is compatible with x86_64 (or amd64), armhf, arm64, and s390x architectures.
+
 ## 1. Update the apt package index and install packages to allow apt to use a repository over HTTPS
 echo "[INFO] Update the apt package index and install packages to allow apt to use a repository over HTTPS"
 sudo apt update
